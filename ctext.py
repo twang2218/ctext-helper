2aeab1030f846f054480ee8baefb733846a71280
Tao Wang <twang2218@gmail.com>
2023-08-18 14:48:20 +1000

    增加直接处理章节链接的能力
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- ctext.py
+++ ctext.py
@@ -33,6 +33,7 @@ s2tm = get_dict(LINK_S2T, with_single_match=False)
 t2s = get_dict(LINK_T2S, with_single_match=True)
 
 def get_book_chapters(url):
+    print(f'正在获取章节列表……')
     content = requests.get(url, timeout=60).content.decode('utf-8')
     # 只获取到域名为止的部分
     base_parts = url.rstrip('/').split('/')
@@ -40,15 +41,18 @@ def get_book_chapters(url):
 
     html = BeautifulSoup(content, 'html.parser')
     chapters = html.select('div.ctext span a')
+    print(chapters)
     if len(chapters) == 0:
         # 可能是「原典全文」中的内容
         chapters = html.select('div#content3 > a')
         if len(chapters) == 0:
             chapters = html.select('div#content2 > a')
     chapters = {a.get_text(): urljoin(baseurl, a['href']) for a in chapters}
+    print(f"共有 {len(chapters)} 章节")
     return chapters
 
 def get_chapter_paragraphs(url):
+    print(f'正在获取段落列表……')
     content = requests.get(url, timeout=60).content.decode('utf-8')
     html = BeautifulSoup(content, 'html.parser')
     paragraphs = html.select('tr.result')
@@ -64,42 +68,30 @@ def get_chapter_paragraphs(url):
         paragraphs = new_paragraphs
     return paragraphs
 
-def check_s2t_multiple(paragraph):
+def check_s2t_multiple(paragraph, chapter='', link='', paragraph_id='', ignore=''):
     candidates = []
     for i, c in enumerate(paragraph):
         if c in t2s:
+            # 将繁体字转换为简体字
             s = t2s[c]
             if s in s2tm:
+                # 寻找简体字对应的多个繁体字（如果存在的话）
                 t2 = s2tm[s]
+                if c in ignore:
+                    # 忽略的字
+                    continue
                 candidates.append({
                     'c': c,
                     't': t2,
-                    'context': paragraph[max(0, i-15):i+15]
+                    'context': paragraph[max(0, i-15):i+15],
+                    'chapter': chapter,
+                    'link': link,
+                    'paragraph_id': paragraph_id,
                 })
+    print(f">    {len(candidates)} 个可能存在错误的繁简转换段落")
     return candidates
 
-def find_error_candidates(url:str, ignore:str=''):
-    if not url.startswith('http'):
-        print(f"Invalid url: {url}")
-        sys.exit(1)
-
-    chapters = get_book_chapters(url)
-    all_candidates = []
-    for chapter, link in chapters.items():
-        print(f"{chapter:<20} \t {link}")
-        paragraphs = get_chapter_paragraphs(link)
-        for paragraph_id, paragraph in paragraphs.items():
-            candidates = check_s2t_multiple(paragraph)
-            for candidate in candidates:
-                if candidate['c'] in ignore:
-                    # 忽略的字
-                    pass
-                else:
-                    candidate['chapter'] = chapter
-                    candidate['link'] = link
-                    candidate['paragraph_id'] = paragraph_id
-                    all_candidates.append(candidate)
-    # 归纳
+def summary(all_candidates):
     candidates = {}
     for candidate in all_candidates:
         if candidate['c'] not in candidates:
@@ -117,8 +109,37 @@ def find_error_candidates(url:str, ignore:str=''):
     keys = list(candidates.keys())
     keys.sort()
     candidates = {k: candidates[k] for k in keys}
-
+    print(f">    共有 {len(candidates)} 个可能存在错误的繁简对")
     return candidates
+
+def find_error_candidates_in_book(url:str, ignore:str=''):
+    if not url.startswith('http'):
+        print(f"Invalid url: {url}")
+        sys.exit(1)
+    print(f'正在获取章节列表……')
+    chapters = get_book_chapters(url)
+    print(f"共有 {len(chapters)} 章节")
+    all_candidates = []
+    for chapter, link in chapters.items():
+        print(f"{chapter:<20} \t {link}")
+        paragraphs = get_chapter_paragraphs(link)
+        for paragraph_id, paragraph in paragraphs.items():
+            candidates = check_s2t_multiple(paragraph, chapter, link, paragraph_id, ignore)
+            all_candidates.extend(candidates)
+    # 归纳
+    return summary(all_candidates)
+
+def find_error_candidates_in_chapter(url:str, ignore:str=''):
+    if not url.startswith('http'):
+        print(f"Invalid url: {url}")
+        sys.exit(1)
+    all_candidates = []
+    paragraphs = get_chapter_paragraphs(url)
+    for paragraph_id, paragraph in paragraphs.items():
+        candidates = check_s2t_multiple(paragraph, '', url, paragraph_id, ignore)
+        all_candidates.extend(candidates)
+    # 归纳
+    return summary(all_candidates)
 
 
 def main():
@@ -128,7 +149,16 @@ def main():
     args.add_argument('--ignore', type=str, default='', help='忽略的字')
     args = args.parse_args()
 
-    candidates = find_error_candidates(args.url, args.ignore)
+    url = args.url.strip()
+    if '&res=' in url or url.endswith('/zh'):
+        # 书籍链接
+        candidates = find_error_candidates_in_book(url, args.ignore)
+    elif 'chapter=' in url:
+        # 章节链接
+        candidates = find_error_candidates_in_chapter(url, args.ignore)
+    else:
+        print('无效的链接。请使用 ctext 的书籍链接或者章节链接。如果是确认链接是正确的，请联系作者。 (QQ：2107553024)')
+        return
 
     # 输出
     if args.file:
@@ -150,7 +180,16 @@ def web():
         ignore = st.text_input('忽略的字')
         st.button('开始检查')
     if url:
-        candidates = find_error_candidates(url, ignore)
+        url = url.strip()
+        if '&res=' in url or url.endswith('/zh'):
+            # 书籍链接
+            candidates = find_error_candidates_in_book(url, ignore)
+        elif 'chapter=' in url:
+            # 章节链接
+            candidates = find_error_candidates_in_chapter(url, ignore)
+        else:
+            st.write('无效的链接。请使用 ctext 的书籍链接或者章节链接。如果是确认链接是正确的，请联系作者。 (QQ：2107553024)')
+            return
         if len(candidates) > 0:
             for v in candidates.values():
                 # st.divider()
